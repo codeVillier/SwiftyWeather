aeb4eff2a9c803bddc50b33f2423b53589300f9f
codeVillier <odevill@icloud.com>
2026-06-03 19:36:07 +0200

    Initial Commit

---
+++ SwiftyWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftyWeather
+//
+//  Created by Oscar De Villiers on 2026/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftyWeather/SwiftyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherApp.swift
+//  SwiftyWeather
+//
+//  Created by Oscar De Villiers on 2026/06/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
